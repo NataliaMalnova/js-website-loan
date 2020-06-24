3edf436ec744a3cd605d22a1cfc335f42eab0825
Natalia <natalia_vileyto@outlook.com>
2020-06-24 17:09:53 +0300

    update sliders

--- src/js/modules/slider/slider-mini.js
+++ src/js/modules/slider/slider-mini.js
@@ -63,19 +63,22 @@ export default class MiniSlider extends Slider{
     }
 
     init() {
-        this.container.style.cssText = `
-            display: flex;
-            flex-wrap: wrap;
-            overflow: hidden;
-            align-items: flex-start;
-            width: 100%
-        `;
+        try{
+            this.container.style.cssText = `
+                display: flex;
+                flex-wrap: wrap;
+                overflow: hidden;
+                align-items: flex-start;
+                width: 100%
+            `;
 
-        this.bindTriggers();
-        this.decorizeSlides();
+            this.bindTriggers();
+            this.decorizeSlides();
 
-        if(this.autoplay) {
-            setInterval(() => this.nextSlide() , 5000);
-        }
+            if(this.autoplay) {
+                setInterval(() => this.nextSlide() , 5000);
+            }
+        } catch(e) {}
+        
     }
 }
